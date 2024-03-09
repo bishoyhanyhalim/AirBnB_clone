5f6a5b6c476302423833715b4a5e4d35c54344b1
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-03-09 11:55:15 -0800

    update base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
 import uuid
 from datetime import datetime
+
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
@@ -7,21 +9,23 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ['updated_at', 'created_at']:
-                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                        setattr(self, key, datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f"))
                     else:
                         setattr(self, key, value)
         else:
-            from models import storage   
+            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
     def save(self):
-        from models import storage  
+        from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
