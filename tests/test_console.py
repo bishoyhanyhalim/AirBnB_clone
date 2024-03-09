597718a8cd5307d420038b22f1cdffcbe79e8364
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-03-10 01:51:57 +0200

    'pycooo'

--- tests/test_console.py
+++ tests/test_console.py
@@ -40,63 +40,63 @@ class TestHBNBCommand(unittest.TestCase):
             self.assertEqual(True, True)
 
     def test_show_missing_class_name(self):
-        """"""
+        """Show test."""
         with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
             HBNBCommand().onecmd("show")
             assert mock_stdout.getvalue() == "** class name missing **\n"
             self.assertEqual(True, True)
 
     def test_show_invalid_class(self):
-        """"""
+        """Show an invalid class test."""
         with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
             HBNBCommand().onecmd("show MyClass")
             assert mock_stdout.getvalue() == "** class doesn't exist **\n"
             self.assertEqual(True, True)
 
     def test_show_missing_id(self):
-        """"""
+        """Show without providing id."""
         with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
             HBNBCommand().onecmd("show BaseModel")
             assert mock_stdout.getvalue() == "** instance id missing **\n"
             self.assertEqual(True, True)
 
     def test_show_no_instance_found(self):
-        """"""
+        """Show invalid instance test."""
         with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
             HBNBCommand().onecmd("show BaseModel 456")
             assert mock_stdout.getvalue() == "** no instance found **\n"
             self.assertEqual(True, True)
 
     def test_destroy_missing_class_name(self):
-        """"""
+        """Destroy test."""
         with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
             HBNBCommand().onecmd("destroy")
             assert mock_stdout.getvalue() == "** class name missing **\n"
             self.assertEqual(True, True)
 
     def test_destroy_invalid_class(self):
-        """"""
+        """Destroy invalid class test."""
         with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
             HBNBCommand().onecmd("destroy MyClass")
             assert mock_stdout.getvalue() == "** class doesn't exist **\n"
             self.assertEqual(True, True)
 
     def test_destroy_missing_id(self):
-        """"""
+        """Destroy missing id test."""
         with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
             HBNBCommand().onecmd("destroy BaseModel")
             assert mock_stdout.getvalue() == "** instance id missing **\n"
             self.assertEqual(True, True)
 
     def test_destroy_no_instance_found(self):
-        """"""
+        """Destroy invalid instance."""
         with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
             HBNBCommand().onecmd("destroy BaseModel 456")
             assert mock_stdout.getvalue() == "** no instance found **\n"
             self.assertEqual(True, True)
 
     def test_do_all_invalid_class(self):
-        """"""
+        """All invalid class test."""
         with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
             HBNBCommand().onecmd("all MyClass")
             assert mock_stdout.getvalue() == "** class doesn't exist **\n"
