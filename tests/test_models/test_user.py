0cacec80d22f3983dd63bd313c48ea0f78aed935
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-03-10 15:12:26 -0700

    user unit test 2

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -9,12 +9,10 @@ class TestUser(unittest.TestCase):
         """the set tests method"""
         self.user = User()
 
-    def test_the_attribute(self):
-        """the attribute values tests method"""
-        self.assertEqual(self.user.email, "")
-        self.assertEqual(self.user.password, "")
-        self.assertEqual(self.user.first_name, "")
-        self.assertEqual(self.user.last_name, "")
+    def test_str_attribute_value(self):
+        """the non-string of user tests method"""
+        with self.assertRaises(TypeError):
+            self.user.email = 707
 
     def test_input_of_attributes(self):
         """the setting tests method"""
@@ -28,26 +26,18 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.user.password, "thisIsPass")
         self.assertEqual(self.user.last_name, "hany")
 
-    def test_does_this_dict(self):
-        """the dictionary of user tests method"""
-        user_dict = self.user.to_dict()
-        self.assertIn('email', user_dict)
-        self.assertIn('created_at', user_dict)
-        self.assertIn('updated_at', user_dict)
-        self.assertIn('last_name', user_dict)
-        self.assertIn('first_name', user_dict)
-        self.assertIn('id', user_dict)
-        self.assertIn('password', user_dict)
-
-    def test_str_attribute_value(self):
-        """the non-string of user tests method"""
-        with self.assertRaises(TypeError):
-            self.user.email = 707
-
     def test_this_attribute(self):
         """the assigning attribute of user tests method"""
         with self.assertRaises(AttributeError):
             self.user.undefined_attribute = "don't work this"
+
+    def test_does_this_dict(self):
+        """the dictionary of user tests method"""
+        user_dict = self.user.to_dict()
+        self.assertIn('email', user_dict)
+        self.assertIn('last_name', user_dict)
+        self.assertIn('first_name', user_dict)
+        self.assertIn('password', user_dict)
 
     def test_the_dict(self):
         """the initializing dictionary of user tests method"""
@@ -64,6 +54,13 @@ class TestUser(unittest.TestCase):
         self.assertEqual(new_user.first_name, "bishoy")
         self.assertEqual(new_user.password, "thisIsPass")
         self.assertEqual(new_user.last_name, "hany")
+
+    def test_the_attribute(self):
+        """the attribute values tests method"""
+        self.assertEqual(self.user.email, "")
+        self.assertEqual(self.user.password, "")
+        self.assertEqual(self.user.first_name, "")
+        self.assertEqual(self.user.last_name, "")
 
 
 if __name__ == '__main__':
